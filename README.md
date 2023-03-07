5cbd204141824bc4e12124d4bada684d2c4f8f14
abmetheny <abmetheny@gmail.com>
2023-03-06 22:35:42 -0500

    validate class/functions working, began populating README

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # svg-logo-maker
 A command-line application that generates an SVG logo based on user input.
+
+## User Story
+AS a freelance web developer  
+I WANT to generate a simple logo for my projects  
+SO THAT I don't have to pay a graphic designer
+
+## Acceptance Criteria
+GIVEN a command-line application that accepts user input  
+WHEN I am prompted for text  
+THEN I can enter up to three characters  
+WHEN I am prompted for the text color  
+THEN I can enter a color keyword (OR a hexadecimal number)  
+WHEN I am prompted for a shape  
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square  
+WHEN I am prompted for the shape's color  
+THEN I can enter a color keyword (OR a hexadecimal number)  
+WHEN I have entered input for all the prompts  
+THEN an SVG file is created named `logo.svg`  
+AND the output text "Generated logo.svg" is printed in the command line  
+WHEN I open the `logo.svg` file in a browser  
+THEN I am shown a 300x200 pixel image that matches the criteria I entered
+
+## Description of Work Performed
+* Imported Inquirer package to allow user interface with terminal
+  * Wrote questions for users to answer in the command-line application
+  * Included Inquirer validation checking to validate user inputs
+* Imported Jest package for test validation
+* Answers to command-line questions are passed into a JavaScript function to generate an SVG file
+  * User-entered text is used as the SVG text
+  * User-selected license is displayed as a clickable banner at the top as well as a clickable link in the License section
+  * GitHub username links to user's GitHub profile page
+  * Email address is clickable and opens a blank email
+  * Table of contents links to relative sections within the README
+
+## Link to deployed GitHub page
+[Deployed Challenge 9 README Generator](https://abmetheny.github.io/readme-generator/)
+
+## Link to walkthrough tutorial
+[README Generator walkthrough](https://drive.google.com/file/d/1J1SiNfNuwHeCyqr6tUaChFOurwLVETRV/view?usp=sharing)
+
+## Screenshots
+<img src="./assets/images/Screenshot1.png">
+<img src="./assets/images/Screenshot2.png">
+<img src="./assets/images/Screenshot3.png">
+
+## License
+This project is licensed under the MIT License - see the LICENSE.md file for details.
+
+# Resources
+- Node packages: [Inquirer](https://www.npmjs.com/package/inquirer)
+- License badges: [Markdown License Badges Repo](https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba)
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // Imports needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Shape = require('./lib/shapes.js');
+const { Shape } = require('./lib/shapes.js');
 const Validate = require('./lib/validate.js')
-// const generateMarkdown = require('./utils/generateMarkdown');
 
 // Array of questions for user input
 const questions = [
@@ -13,10 +12,10 @@ const questions = [
         message: 'Enter up to 3 characters for SVG text.',
         validate: (answerString) => {
             const validate = new Validate(answerString);
-            if(!validate.isText() || !validate.isLength()){
-                console.log("\n Please enter SVG text up to 3 characters.")
-                return false
-            } return true
+            if(!validate.isText() || !validate.isLength(answerString)){
+                console.log("\n Please enter SVG text up to 3 characters.");
+                return false;
+            } return true;
         }
     },
     {
@@ -24,11 +23,11 @@ const questions = [
         name: 'textcolor',
         message: 'Enter a color/hex number for your text.',
         validate: (answerString) => {
-            const validate = new Validate(answerString);
-            if(!validate.isTextColor() || !validate.isLength()){
-                console.log("\n Please enter a valid color.")
-                return false
-            } return true
+            const validate = new Validate("text", answerString);
+            if(!validate.isTextColor()){
+                console.log("\n Please enter a valid color.");
+                return false;
+            } return true;
         }
     },
     {
@@ -42,18 +41,18 @@ const questions = [
         name: 'shapecolor',
         message: 'Enter a color/hex number for your shape.',
         validate: (answerString) => {
-            const validate = new Validate(answerString);
-            if(!validate.isTextColor() || !validate.isLength()){
-                console.log("\n Please enter a valid color.")
-                return false
-            } return true
+            const validate = new Validate("text", "textcolor", answerString);
+            if(!validate.isShapeColor()){
+                console.log("\n Please enter a valid color.");
+                return false;
+            } return true;
         }
     },
 ];
 
 // Function to write to SVG file
 function writeToFile(svgContent) {
-    fs.writeFile(`logo.svg`, svgContent, (err) => {
+    fs.writeFile(`./examples/logo.svg`, svgContent, (err) => {
                 if (err) throw err;
                 console.log("Saved!");
             });
@@ -64,16 +63,13 @@ function init() {
     inquirer
         .prompt(questions)
         .then((answers) => {
-            // console.log(answers);
+            console.log(answers);
             const text = answers.text;
             const textfill = answers.textcolor;
             const shape = answers.shape;
             const shapefill = answers.shapecolor;
 
             const newShape = new Shape(text, textfill, shape, shapefill);
-            console.log(newShape);
-
-            console.log(newShape.renderShape(text, textfill, shape, shapefill));
             
             const svgContent = newShape.renderShape(text, textfill, shape, shapefill);
             writeToFile(svgContent);
